27f901e614a3019f839d5e6d88cb8f1332bb65b0
kulmurzaeva84 <kjika84@gmail.com>
2020-08-04 19:10:23 -0500

    update day38

---
+++ src/day38_JavaRecap/GradeReport.java
@@ -0,0 +1,54 @@
+package day38_JavaRecap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GradeReport {
+    public static void main(String[] args) {
+         /*
+        how many students made:
+                A?
+                B?
+                C?
+                D?
+        how many failed?
+        DO NOT use loop
+         */
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll( Arrays.asList(100, 90, 75, 85, 65, 85, 55, 45, 73, 73, 35, 47));
+
+        ArrayList<Integer> gradeA = new ArrayList<>(); // 90 ~ 100
+        gradeA.addAll(list);  // first we store all the grades
+        gradeA.removeIf(p -> p < 90 );  // then we remove the grades that are not A
+        System.out.println("Grade A: "+ gradeA);
+
+        ArrayList<Integer> gradeB = new ArrayList<>(); // 80 ~ 89
+        gradeB.addAll(list);   // first we store all the grades
+        gradeB.removeIf( each -> each <80 || each > 89 );  // then we remove the grades that are not B
+        System.out.println("Grade B: "+gradeB);
+
+
+        ArrayList<Integer> gradeC = new ArrayList<>(); // 70 ~ 79
+        gradeC.addAll(list);
+        gradeC.removeIf(p -> p<70 || p>79);
+        System.out.println("Grade C: "+ gradeC);
+
+
+        ArrayList<Integer> gradeD = new ArrayList<>(); // 60 ~ 69
+        gradeD.addAll(list);
+        gradeD.removeIf(p -> p<60 || p>69);
+        System.out.println("Grade D:" + gradeD);
+
+
+        ArrayList<Integer> gradeF = new ArrayList<>(); // 0 ~ 59
+        gradeF.addAll(list);
+        gradeF.removeAll(gradeA); // OR gradeF.removeIf(p -> p > 59);
+        gradeF.removeAll(gradeB);
+        gradeF.removeAll(gradeC);
+        gradeF.removeAll(gradeD);
+        System.out.println("Grade F:"+ gradeF);
+
+
+
+    }
+}
